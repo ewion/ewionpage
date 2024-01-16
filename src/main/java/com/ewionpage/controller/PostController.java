7c90531b7dd4eb9cc7104566cdb9ec40904c6b39
ewion <dnznjt05@gmail.com>
2024-01-17 00:45:07 +0900

    240117 blog

--- src/main/java/com/ewionpage/controller/PostController.java
+++ src/main/java/com/ewionpage/controller/PostController.java
@@ -1,13 +1,27 @@
 package com.ewionpage.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.ewionpage.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 public class PostController {
+    // HTTP methods
+    // GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD, TRACE, CONNECT
+    // 글 등록 : POST method
 
-    @GetMapping("/posts")
-    public String get() {
+    @PostMapping("/posts")
+    public String post(PostCreate params) {
+        // log.info("title={}, content={}",title, content);
+        log.info("params={}",params);
         return "Hello World!";
+
     }
 }
--- src/test/java/com/ewionpage/controller/PostControllerTest.java
+++ src/test/java/com/ewionpage/controller/PostControllerTest.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -22,7 +24,15 @@ class PostControllerTest {
     @Test
     @DisplayName("/posts 요청 시 Hello world 출력")
     void test() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+        // 글 제목
+        // 글 내용
+        // MockMvc 는 json 형태로 전송
+        // expected
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .param("title", "글 제목...")
+                        .param("content", "글 내용...")
+                )  // appication/json형태로 보냄
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World!"))
                 .andDo(print());
