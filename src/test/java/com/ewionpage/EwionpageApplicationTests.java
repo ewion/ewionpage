d6c634669344d1e34234915b8edd2bc20f3dc720
ewion <dnznjt05@gmail.com>
2024-01-16 20:12:46 +0900

    240116 blog

---
+++ src/test/java/com/ewionpage/EwionpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewionpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwionpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/ewionpage/controller/PostControllerTest.java
@@ -0,0 +1,34 @@
+package com.ewionpage.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class PostControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/posts 요청 시 Hello world 출력")
+    void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"))
+                .andDo(print());
+
+
+
+    }
+
+}
